04033a8368c75995c7e548b2db5a6d8a95970b91
KingGeorge <asherlee.bxl@gmail.com>
2026-03-17 09:36:11 +0800

    update

--- CMS/update/Update.h
+++
@@ -1,9 +0,0 @@
-struct Student 
-{
-    int id;
-    char name[255];
-    char programme[255];
-    float mark;
-};
-struct Student *openfile(const char *file_path, int *out_count);
-https://github.com/KingGorgeOfIRL/SIT-PRG-C-PROJECT/issues
