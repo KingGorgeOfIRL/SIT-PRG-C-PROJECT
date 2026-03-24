ce45ace969d3a4c9d4f65a7f1fd44f86d690d40f
KingGeorge <asherlee.bxl@gmail.com>
2026-03-24 14:39:18 +0800

    insert, binary search/query,open_file, update and delete for new data structure

--- CMS/update/update.c
+++ CMS/update/update.c
@@ -3,143 +3,188 @@
 #include <stdlib.h>
 #include <string.h>
 
-// single element record
-typedef struct Student {
+// linked list to store student details
+typedef struct Student{
     int id;
     char name[255];
     char programme[255];
     float mark;
 }Student;
 
-// open IO function to read from txt file
-Student *openfile(const char *file_path, int *out_count){
-    FILE *fptr = fopen(file_path,"r");
-    Student *student_records = NULL;
-    int count = 0, capacity = 0;
+// array of linked list pointers, point to the first node of every 10 IDs
+typedef struct Record{
+    Student *node;
+}Record;
 
-    if (out_count != NULL){
-        *out_count = 0;
+// returns position if found and sorted, expected position if not found 
+int binary_search(Record *student_record,int *count, int id){
+/* binary search of array */
+    int low = 0;
+    int high = *count - 1;
+
+    while(low <= high){
+        int position = low + (high - low) / 2;
+        int current_id = student_record[position].node->id;
+        if (current_id == id){
+            return position;
+        }else if (current_id < id){
+            low = position + 1;
+        }else{
+            high = position - 1;
+        }
+    }
+    return low;
+}
+
+// assumes that student is not in record
+int insert(Record **student_record, int *capacity, int *count, int position, int id, const char *name, const char *programme,float mark){
+    /* expanding memory allocation */
+    if (*capacity == *count){
+        int new_cap = *capacity + 1;
+        Record *temp = realloc(*student_record,new_cap * sizeof((*student_record)[0]));
+        if (temp == NULL){
+            return -1;
+        }
+        *student_record = temp;
+        *capacity = new_cap;
     }
 
-    // checks file pointer is vaild
-    if(fptr != NULL){
-        while (1){
-            int id = 0;
-            char name[255] = "";
-            char programme[255] = "";
-            float mark = 0.0f;
-            int result = fscanf(fptr, "%d,%254[^,],%254[^,],%f", &id, name, programme, &mark);
-            if (result == 4){
-                if (count == capacity){
-                    int new_cap = (capacity == 0) ? 2 : capacity * 2;
-                    Student *temp = realloc(student_records,new_cap * sizeof(*student_records));
-                    if (temp == NULL){
-                        free(student_records);
-                        fclose(fptr);
-                        return NULL;
-                    }
-                    student_records = temp;
-                    capacity = new_cap;
-                }
-                student_records[count].id = id;
-                strncpy(student_records[count].name, name, sizeof(student_records[count].name) - 1);
-                student_records[count].name[sizeof(student_records[count].name) - 1] = '\0';
+    /* create new student*/
+    Student *new_student = malloc(sizeof(Student));
+    if (new_student == NULL) {
+        return -1;
+    }else{
+        new_student->mark=mark;
+        new_student->id=id;
+        strcpy(new_student->name,name);
+        strcpy(new_student->programme,programme);
+    }
+    
 
-                strncpy(student_records[count].programme, programme, sizeof(student_records[count].programme) - 1);
-                student_records[count].programme[sizeof(student_records[count].programme) - 1] = '\0';
+    /* create first address */
+    for ( int i = *count; i > position; i--){
+        (*student_record)[i].node = (*student_record)[i-1].node;
+    }
+    (*student_record)[position].node = new_student;
+    (*count)++;
+    return 1;
+}
 
-                student_records[count].mark = mark;
-                count++;
-            }else if (result == EOF){
-                break;
-            }else{
-                int ch;
-                while ((ch = fgetc(fptr)) != '\n' && ch != EOF) {
-                }
-            }
+// open IO function to read from txt file
+Record *openfile(const char *file_path, char err_msg[255], int *count){
+    FILE *fptr = fopen(file_path,"r");
+    int capacity = 1;
+    *count = 0;
 
+    /* invalid file*/
+    Record *student_records = malloc(capacity * sizeof(Record));
+    if (student_records == NULL) {
+        strcpy(err_msg, "Initial memory allocation failure");
+        if (fptr != NULL) {
+            fclose(fptr);
         }
-        // loops through file, line by line of a buffer size of 255 and will break if a newline character is found
-
-    } else{
+        return NULL;
+    }else if (fptr == NULL) {
+        strcpy(err_msg, "Failed to open file");
+        free(student_records);
         return NULL;
     }
+
+    int result;
+    /* reads fileline for variables */
+    int id = 0;
+    char name[255] = "";
+    char programme[255] = "";
+    float mark = 0.0f;
+    do{
+        result = fscanf(fptr, "%d,%254[^,],%254[^,],%f", &id, name, programme, &mark);
+
+        if (result == 4){
+            int position = binary_search(student_records,count,id);
+            result = insert(&student_records,&capacity,count,position,id,name,programme,mark);
+            if (result == -1){
+                strcpy(err_msg, "Expanded memory allocation failure");
+                fclose(fptr);
+                free(student_records);
+                return NULL;
+            }else if (result == 0){
+                strcpy(err_msg, "Unexpected failure");
+                fclose(fptr);
+                free(student_records);
+                return NULL;
+            }
+        }else{
+            int ch;
+            while ((ch = fgetc(fptr)) != '\n' && ch != EOF) {
+            }
+        }
+    }while (result != EOF);
     fclose(fptr);
-    if (out_count != NULL) {
-        *out_count = count;
-    }
     return student_records;
 }
 
-// generic bool check of 
-int check_query(int id, Student *student_record,int size){
-    for (int i = 0; i < size; i++){
-        if (student_record[i].id == id){
-            return i;
-        }
-    }
-    return sizeof(student_record) + 1;
-}
-
-int update_student(Student *student_record, int *size_of_array,Student *new_data){
-    int position = check_query(new_data[0].id,student_record,*size_of_array);
-    if (position <= *size_of_array){
-        student_record[position].id = new_data[0].id;
-        strcpy(student_record[position].name,new_data[0].name);
-        strcpy(student_record[position].programme,new_data[0].programme);
-        student_record[position].mark = new_data[0].mark;
+// returns student id if there is an error, assumes that student is in record
+int update(Record *student_record, int size_of_array, int position, Student *new_data){
+    if (position <= size_of_array){
+        Student *old_student = student_record[position].node;
+        old_student->id = new_data->id;
+        strcpy(old_student->name,new_data->name);
+        strcpy(old_student->programme,new_data->programme);
+        old_student->mark = new_data->mark;
         return -1;
     }else{
         /* error message of invalid id*/
-        return new_data[0].id;
+        return new_data->id;
     }
 
 }
 
-int delete_student(int id,Student *student_record,int *size_of_array){
-    int i = check_query(id,student_record,*size_of_array);
-    if (i <= *size_of_array){
-        int count = *size_of_array;
-        for (i; i < count; i++) student_record[i] = student_record[i+1];
-        *size_of_array = count - 1;
+int delete(int position, Record *student_record,int size_of_array){
+    if (position <= size_of_array){
+        free(student_record[position].node);
+        for (int i = position; i < size_of_array; i++){
+            student_record[i].node = student_record[i+1].node;
+        }
         return -1;
     }else{
-        return id;
+        return position;
     }
 }
 
-void show_all(Student *records,int count){
+void show_all(Record *records,int count){
     for (int i = 0; i < count; i++) {
-    printf("%d, %s, %s, %.2f\n\n",
-        records[i].id,
-        records[i].name,
-        records[i].programme,
-        records[i].mark);
+    Student *current = records[i].node;
+    if (current == NULL){
+        continue;
     }
+    
+    printf("%d, %s, %s, %.2f\n",
+        current->id,
+        current->name,
+        current->programme,
+        current->mark);
+    }
+    printf("\n");
 }
 
 int main(){    
-    int id,err,count = 0;
-    Student *records = openfile("../../DB/Sample_CMS.txt", &count);
-    Student update_data = {id,"testname","testprogramme",11.11};
+    int id = 2304567,count = 0,capacity = 1;
+    char err_msg[255];
+    Record *records = openfile("../../StudentRecords/P1-7_CMS.txt",err_msg, &count);
     if (records != NULL) {
-        show_all(records,count);
-        int id;
-        printf("enter ID to update\n");
-        scanf("%d",&id);
-        update_data.id = id;
-        err = update_student(records,&count,&update_data);
-        if (err == -1){
-            show_all(records,count);
-        }else printf("%d\n",err);
-        
-        printf("enter ID to delete\n");
-        scanf("%d",&id);
-        err = delete_student(id,records,&count);
-        if (err == -1){
-            show_all(records,count);
-        }else printf("%d\n",err);        
-    }
+       Student update_data = {id,"testname","testprogramme",11.11};
+       int position = binary_search(records,&count, id);
+       show_all(records,count);
+
+       update(records,count,position,&update_data);
+       show_all(records,count);
+
+       delete(position,records,count);
+       show_all(records,count);
+       
+       insert(&records,&capacity,&count,position,id,update_data.name,update_data.programme,update_data.mark);
+       show_all(records,count);
+    }  
 
     free(records);
     return 0;
