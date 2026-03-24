746dd2d99289e7f0efa46bffc63ddd6b3210d6c9
Dr00led <raytanjuntao20@gmail.com>
2026-03-24 01:14:28 -0400

    Changes made: - Edited open.c and save.c functions to use the same naming scheme as the other functions in commands.c - Added a new io.h file that includes the functions in open.c and save.c, so that commands.c knows where to get the functions from - Edited open.c and save.c to use the new database record P1-7_CMS.txt instead

--- assets/commands.c
+++ assets/commands.c
@@ -2,6 +2,7 @@
 #include "../CMS/commands/db_func.h"
 #include "../CMS/insert/insert_func.h"
 #include "../CMS/read/read.h"
+#include "../CMS/IO/io.h"
 
 // command table
 Command commands[] = {
@@ -10,8 +11,8 @@ Command commands[] = {
     {"insert", cmd_insert, "Insert new entry"},
     {"showall", cmd_showall, "Display all records"},
     {"query",   cmd_query,   "Search for a record"},
-    {"open",   cmd_loadStudents,   "Opens the database record"},
-    {"save",   cmd_saveStudents,   "Saves to the database record"}
+    {"open",   cmd_loadStudents,   "Opens the database file and read in all the records"},
+    {"save",   cmd_saveStudents,   "Saves all the current records into the database file"}
 
 };
 
